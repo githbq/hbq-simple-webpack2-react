7ff67aff2fc6af2835d0622c2ef7449b1900f61e
hubiqing01 <hubiqing01@58ganji.com>
2017-05-19 17:37:22 +0800

    polyfillComment

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 /**
  * 程序的入口 
  */
-import 'babel-polyfill'; //es6
+// import 'babel-polyfill'; //es6
 import React from 'react';
 import update from 'react-addons-update';
 //追加功能库 用来操作多层state
 React.AddonsUpdate = update;
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'; 
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 //页面
 import DemoRoute from '@/routes/Demo';
 // 路由表 
