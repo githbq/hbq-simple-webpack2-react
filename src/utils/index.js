be7ab2078628a611e39c8fba012fdb5bd8702f87
hubiqing01 <hubiqing01@58ganji.com>
2017-06-20 15:53:37 +0800

    lazyload

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 import ajax from './ajax';
 import lazyload from './lazyload';
 import momentHelper from './momentHelper';
-export default { ajax, lazyLoad, momentHelper };
+export default { ajax, lazyload, momentHelper };
