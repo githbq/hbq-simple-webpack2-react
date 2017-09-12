209387a40551c109b35aa1e17368f9a4e5f85452
hubiqing <hubiqing01@58ganji.com>
2017-09-12 21:33:26 +0800

    ok

--- src/routes/Demo/index.jsx
+++ src/routes/Demo/index.jsx
@@ -6,10 +6,20 @@ import React, { PropTypes } from 'react';
 import DemoComponent from '@/components/Demo';
 export default class extends React.Component {
     render() {
+
         return <div className="demo-route">
             this is demo-route
             <DemoComponent />
         </div>
     }
+}
 
-} 
+async function b() {
+    return '432432423'
+}
+async function a() {
+    const x = await b()
+    console.log('xzxxxxx', x)
+}
+
+a()
