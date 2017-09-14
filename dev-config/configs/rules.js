f0d91512a92e4b6c3c52277cdc3b731376694f89
hubiqing <hubiqing01@58ganji.com>
2017-09-14 21:54:41 +0800

    ok

--- dev-config/configs/rules.js
+++ dev-config/configs/rules.js
@@ -6,60 +6,60 @@ const { TEMPLATE_PATH, PUBLIC_PATH, ROOT_PATH, APP_PATH, BUILD_PATH, NODE_ENV, _
 const lessLoaderVars = {};
 // 将babel-loader的配置独立出来, 因为webpack的限制: http://stackoverflow.com/questions/33117136/how-to-add-a-query-to-a-webpack-loader-with-multiple-loaders
 const babelLoaderConfig = {
-    presets: ['latest', 'stage-0', 'react'], // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
-    plugins: [
-        ['import', { libraryName: 'antd', style: true }],
-        'add-module-exports',
-        'transform-runtime']
+  presets: ['env', 'stage-0', 'react'], // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
+  plugins: [
+    ['import', { libraryName: 'antd', style: true }],
+    'add-module-exports',
+    'transform-runtime']
 };
 const postCSSConfig = JSON.stringify(require('./utils').postCSSConfig);
-let rules = [ // 定义各种loader  
-    {
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif|mp4|webm)$/,
-        use: [{
-            loader: 'url-loader',
-            options: { limit: 8192, name: 'assets/generates/[hash].[ext]' }
-        }]
-    },
-    {
-        test: /\.(js|jsx)$/,
-        use: ['source-map-loader'],
-        exclude: /(node_modules)/,
-        enforce: 'pre'
-    },
-    ...require('./rulesOfCss')({
-        __DEV__,
-        lessLoaderVars,
-        postCSSConfig
-    }, false, !__DEV__)
+let rules = [ // 定义各种loader
+  {
+    test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif|mp4|webm)$/,
+    use: [{
+      loader: 'url-loader',
+      options: { limit: 8192, name: 'assets/generates/[hash].[ext]' }
+    }]
+  },
+  {
+    test: /\.(js|jsx)$/,
+    use: ['source-map-loader'],
+    exclude: /(node_modules)/,
+    enforce: 'pre'
+  },
+  ...require('./rulesOfCss')({
+    __DEV__,
+    lessLoaderVars,
+    postCSSConfig
+  }, false, !__DEV__)
 ];
 
 if (__DEV__) {
-    rules.push({
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: [
-            { loader: 'react-hot-loader' },
-            {
-                loader: 'babel-loader',
-                options: Object.assign(babelLoaderConfig, { cacheDirectory: true })
-            }
-        ]
-    });
+  rules.push({
+    test: /\.(js|jsx)$/,
+    exclude: /(node_modules)/,
+    use: [
+      { loader: 'react-hot-loader' },
+      {
+        loader: 'babel-loader',
+        options: Object.assign(babelLoaderConfig, { cacheDirectory: true })
+      }
+    ]
+  });
 } else {
-    //生产环境
-    rules.push({
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: [{
-            loader: 'babel-loader',
-            options: babelLoaderConfig,
-        },
-        {
-            loader: 'strip-loader',
-            options: { strip: ['logger.info', 'logger.debug', 'console.log', 'console.debug'] }
-        }
-        ]
-    });
+  //生产环境
+  rules.push({
+    test: /\.(js|jsx)$/,
+    exclude: /(node_modules)/,
+    use: [{
+      loader: 'babel-loader',
+      options: babelLoaderConfig,
+    },
+    {
+      loader: 'strip-loader',
+      options: { strip: ['logger.info', 'logger.debug', 'console.log', 'console.debug'] }
+    }
+    ]
+  });
 }
 module.exports = rules;
