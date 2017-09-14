864a17533e6ad350342f1873ae3e6fedac3b98e7
hubiqing <hubiqing01@58ganji.com>
2017-09-14 21:57:18 +0800

    ok

--- dev-config/configs/rules.js
+++ dev-config/configs/rules.js
@@ -6,7 +6,7 @@ const { TEMPLATE_PATH, PUBLIC_PATH, ROOT_PATH, APP_PATH, BUILD_PATH, NODE_ENV, _
 const lessLoaderVars = {};
 // 将babel-loader的配置独立出来, 因为webpack的限制: http://stackoverflow.com/questions/33117136/how-to-add-a-query-to-a-webpack-loader-with-multiple-loaders
 const babelLoaderConfig = {
-  presets: ['env', 'react'], // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
+  presets: ['env', 'stage-0', 'react'], // 开启ES6、部分ES7、react特性, preset相当于预置的插件集合
   plugins: [
     ['import', { libraryName: 'antd', style: true }],
     'add-module-exports',
