ba159f4001bfae747782fa9ac16cd2b0df486ada
hbq <hbqhbq123@qq.com>
2017-09-23 11:36:05 +0800

    port

--- dev-config/configs/devServer.js
+++ dev-config/configs/devServer.js
@@ -3,22 +3,24 @@ const { TEMPLATE_PATH, PUBLIC_PATH, ROOT_PATH, APP_PATH, BUILD_PATH, NODE_ENV, _
  * 开发服务配置
  */
 module.exports = {
-    hot: true,
-    open: true,
-    port: 7001,
-    contentBase: BUILD_PATH,
-    publicPath: PUBLIC_PATH,
-    inline: true,
-    // clientLogLevel: 'warning', // none, error, warning or info (default).
-    // HTML5 History API
-    historyApiFallback: true,
-    // watchOptions: {
-    //     aggregateTimeout: 300, // rebuild 延时, wait so long for more changes
-    //     ignored: /node_modules/,
-    //     poll: 1000, // Check for changes every second
-    // },
-    quiet: true, // 让dev server处于静默的状态启动(控制台中不输出打包的信息)
-    noInfo: false, // set to false to see a list of every file being bundled.
-    headers: { 'X-Custom-Header': 'yes' },
-    proxy: require('./proxy')
+  openPage: 'index.html',
+  overlay: true,
+  hot: true,
+  open: true,
+  port: Math.floor(Math.random() * 1001) + 7000,
+  contentBase: BUILD_PATH,
+  publicPath: PUBLIC_PATH,
+  inline: true,
+  // clientLogLevel: 'warning', // none, error, warning or info (default).
+  // HTML5 History API
+  historyApiFallback: true,
+  // watchOptions: {
+  //     aggregateTimeout: 300, // rebuild 延时, wait so long for more changes
+  //     ignored: /node_modules/,
+  //     poll: 1000, // Check for changes every second
+  // },
+  quiet: true, // 让dev server处于静默的状态启动(控制台中不输出打包的信息)
+  noInfo: false, // set to false to see a list of every file being bundled.
+  headers: { 'X-Custom-Header': 'yes' },
+  proxy: require('./proxy')
 };
